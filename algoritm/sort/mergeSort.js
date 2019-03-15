02a96c014abe3977c7b77de222bc86813479e4b6
“Jung <smokerjs@stunitas.com>
2019-03-15 16:51:39 +0900

    day3 - mergeSort

--- algoritm/sort/mergeSort.js
+++ algoritm/sort/mergeSort.js
@@ -1,6 +1,6 @@
 const arr = [13,5,74,6,3,6,7,2,25,63,12,45,1,0,2,5,7,9,3];
 
-
+// 시간복잡도 O(nlogn)
 function mergeSort(arr) {
   if (arr.length <= 1) return arr;
   let pivot = Math.floor(arr.length / 2);
